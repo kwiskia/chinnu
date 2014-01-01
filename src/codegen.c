fa1ba207ac24e8253f847caffb8e4005deef9916
Eric Fritz <eric@eric-fritz.com>
2014-01-01 16:06:53 -0600

    Remove global temporary count.

--- src/codegen.c
+++ src/codegen.c
@@ -131,61 +131,45 @@ int get_upvar_index(Scope *scope, Symbol *symbol) {
     return -1;
 }
 
-static int temp = 0;
-static int maxt = 0;
-
-void _mktemp() {
-    temp++;
-
-    if (temp > maxt) {
-        maxt = temp;
-    }
-}
-
-void _rmtemp() {
-    temp--;
-}
-
-int get_temp_index(Scope *scope) {
+int get_temp_index(Scope *scope, int temp) {
     return scope->numlocals + temp + 1;
 }
 
 /* forward */
-void compile_list(ExpressionList *list, Chunk *chunk, Scope *scope, int dest);
+int compile_list(ExpressionList *list, Chunk *chunk, Scope *scope, int dest, int temp);
 
-void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
+int compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest, int temp) {
     switch (expr->type) {
         case TYPE_MODULE:
-            compile_expr(expr->lexpr, chunk, scope, dest);
+        {
+            int max = compile_expr(expr->lexpr, chunk, scope, dest, temp);
             add_instruction(chunk, CREATE(OP_RETURN, 0, 0, 0));
-            break;
+
+            return max;
+        }
 
         case TYPE_DECLARATION:
+        {
             if (expr->rexpr) {
-                compile_expr(expr->rexpr, chunk, scope, dest);
+                int max = compile_expr(expr->rexpr, chunk, scope, dest, temp);
                 // TODO - peephole optimize
                 add_instruction(chunk, CREATE(OP_MOVE, get_local_index(scope, expr->symbol), dest, 0));
+
+                return max;
             }
-            break;
+
+            return temp;
+        }
 
         case TYPE_FUNC:
         {
-            int temp1 = temp;
-            int temp2 = maxt;
-
-            temp = 0;
-            maxt = 0;
-
             Chunk *child = make_chunk();
-            compile_expr(expr->rexpr, child, expr->scope, 0);
+            int max = compile_expr(expr->rexpr, child, expr->scope, 0, 0);
 
-            child->numtemps = maxt;
+            child->numtemps = max;
             child->numlocals = expr->scope->numlocals;
             child->numupvars = expr->scope->numupvars;
             child->numparams = expr->scope->numparams;
-
-            temp = temp1;
-            maxt = temp2;
 
             int index = add_func_child(chunk, child);
             add_instruction(chunk, CREATE(OP_CLOSURE, dest, index, 0));
@@ -206,12 +190,14 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
                 int index = get_local_index(scope, expr->symbol);
                 add_instruction(chunk, CREATE(OP_MOVE, index, dest, 0));
             }
-        } break;
+
+            return temp;
+        }
 
         case TYPE_CALL:
         {
             // compile receiver
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
             /**
              * TODO
@@ -224,24 +210,27 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
             if (expr->llist->head == NULL) {
                 add_instruction(chunk, CREATE(OP_CALL, dest, dest, 0));
             } else {
-                int backup = temp;
-                int f = get_temp_index(scope);
+                int f = get_temp_index(scope, temp);
                 int t = f;
 
                 ExpressionNode *head;
                 for (head = expr->llist->head; head != NULL; head = head->next) {
-                    _mktemp();
-                    compile_expr(head->expr, chunk, scope, t);
+                    int n = compile_expr(head->expr, chunk, scope, t, ++temp);
+
+                    if (n > max) {
+                        max = n;
+                    }
 
                     if (head->next != NULL) {
-                        t = get_temp_index(scope);
+                        t = get_temp_index(scope, temp);
                     }
                 }
 
-                temp = backup;
                 add_instruction(chunk, CREATE(OP_CALL, dest, dest, f));
             }
-        } break;
+
+            return temp;
+        }
 
         case TYPE_VARREF:
         {
@@ -253,7 +242,9 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
                 index = get_upvar_index(scope, expr->symbol);
                 add_instruction(chunk, CREATE(OP_GETUPVAR, dest, index, 0));
             }
-        } break;
+
+            return temp;
+        }
 
         /* control flow */
         case TYPE_IF:
@@ -266,19 +257,20 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
             // [nm]
 
             // condition
-            compile_expr(expr->cond, chunk, scope, dest);
+            int max1 = compile_expr(expr->cond, chunk, scope, dest, temp);
 
             // dummy jump 1
             int t1 = add_instruction(chunk, 0);
 
             // true branch
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max2 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
             // dummy jump 2
             int t2 = add_instruction(chunk, 0);
 
+            int max3 = 0;
             if (expr->rexpr) {
-                compile_expr(expr->rexpr, chunk, scope, dest);
+                max3 = compile_expr(expr->rexpr, chunk, scope, dest, temp);
             } else {
                 int index = add_constant(chunk, make_constant(CONST_NULL, expr->value));
                 add_instruction(chunk, CREATE(OP_MOVE, dest, index + 256, 0));
@@ -291,6 +283,12 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             // fill in 2nd jump
             chunk->instructions[t2] = CREATE(OP_JUMP, 0, nm - t2 - 1, 0);
+
+            if (max1 > max2) {
+                return max3 > max1 ? max3 : max1;
+            } else {
+                return max3 > max2 ? max3 : max2;
+            }
         } break;
 
         case TYPE_WHILE:
@@ -302,13 +300,13 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
             // [t3] null
 
             int t0 = chunk->numinstructions;
-            compile_expr(expr->cond, chunk, scope, dest);
+            int max1 = compile_expr(expr->cond, chunk, scope, dest, temp);
 
             // dummy jump 1
             int t1 = add_instruction(chunk, 0);
 
             // compile body
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max2 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
             // dummy jump 2
             int t2 = add_instruction(chunk, 0);
@@ -322,12 +320,14 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             // fill in 2nd jump
             chunk->instructions[t2] = CREATE(OP_JUMP, 0, t3 - t0, 1);
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         /* binary cases */
         case TYPE_ASSIGN:
         {
-            compile_expr(expr->rexpr, chunk, scope, dest);
+            int max = compile_expr(expr->rexpr, chunk, scope, dest, temp);
 
             int index = get_local_index(scope, expr->lexpr->symbol);
 
@@ -338,59 +338,61 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
                 index = get_upvar_index(scope, expr->lexpr->symbol);
                 add_instruction(chunk, CREATE(OP_SETUPVAR, dest, index, 0));
             }
-        } break;
+
+            return max;
+        }
 
         case TYPE_ADD:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_ADD, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_SUB:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_SUB, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_MUL:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_MUL, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_DIV:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_DIV, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         /**
          * optimization - boolean-resulting commands should have a second
@@ -400,82 +402,82 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
         case TYPE_EQEQ:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_EQ, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_NEQ:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_EQ, dest, dest, t));
             add_instruction(chunk, CREATE(OP_NOT, dest, dest, 0));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_LT:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_LT, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_LEQ:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_LE, dest, dest, t));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_GT:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_LT, dest, t, dest));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_GEQ:
         {
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
-            int t = get_temp_index(scope);
-            _mktemp();
-            compile_expr(expr->rexpr, chunk, scope, t);
-            _rmtemp();
+            int t = get_temp_index(scope, temp);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, t, temp + 1);
 
             // TODO - binops with constants without load
             add_instruction(chunk, CREATE(OP_LE, dest, t, dest));
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_AND:
         {
@@ -502,13 +504,13 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
             // [t3] load false
 
             // lexpr
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
             // dummy jump
             int t1 = add_instruction(chunk, 0);
 
             // rexpr
-            compile_expr(expr->rexpr, chunk, scope, dest);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, dest, temp);
 
             // dummy jump
             int t2 = add_instruction(chunk, 0);
@@ -524,7 +526,9 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             // fill in 2nd jump
             chunk->instructions[t2] = CREATE(OP_JUMP_FALSE, dest, t3 - t2 - 1, 0);
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         case TYPE_OR:
         {
@@ -551,13 +555,13 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
             // [t3] load true
 
             // lexpr
-            compile_expr(expr->lexpr, chunk, scope, dest);
+            int max1 = compile_expr(expr->lexpr, chunk, scope, dest, temp);
 
             // dummy jump
             int t1 = add_instruction(chunk, 0);
 
             // rexpr
-            compile_expr(expr->rexpr, chunk, scope, dest);
+            int max2 = compile_expr(expr->rexpr, chunk, scope, dest, temp);
 
             // dummy jump
             int t2 = add_instruction(chunk, 0);
@@ -573,18 +577,24 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             // fill in 2nd jump
             chunk->instructions[t2] = CREATE(OP_JUMP_FALSE, dest, t3 - t2 - 1, 0);
-        } break;
+
+            return max1 > max2 ? max1 : max2;
+        }
 
         /* unary cases */
         case TYPE_NEG:
-            compile_expr(expr->lexpr, chunk, scope, dest);
+        {
+            int max = compile_expr(expr->lexpr, chunk, scope, dest, temp);
             add_instruction(chunk, CREATE(OP_NEG, dest, dest, 0));
-            break;
+            return max;
+        }
 
         case TYPE_NOT:
-            compile_expr(expr->lexpr, chunk, scope, dest);
+        {
+            int max = compile_expr(expr->lexpr, chunk, scope, dest, temp);
             add_instruction(chunk, CREATE(OP_NOT, dest, dest, 0));
-            break;
+            return max;
+        }
 
         /* constants */
         case TYPE_INT:
@@ -599,7 +609,8 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             int index = add_constant(chunk, make_constant(CONST_INT, value));
             add_instruction(chunk, CREATE(OP_MOVE, dest, index + 256, 0));
-        } break;
+            return temp;
+        }
 
         case TYPE_REAL:
         {
@@ -613,7 +624,8 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             int index = add_constant(chunk, make_constant(CONST_REAL, value));
             add_instruction(chunk, CREATE(OP_MOVE, dest, index + 256, 0));
-        } break;
+            return temp;
+        }
 
         case TYPE_BOOL:
         {
@@ -627,7 +639,8 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             int index = add_constant(chunk, make_constant(CONST_BOOL, value));
             add_instruction(chunk, CREATE(OP_MOVE, dest, index + 256, 0));
-        } break;
+            return temp;
+        }
 
         case TYPE_NULL:
         {
@@ -639,7 +652,8 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             int index = add_constant(chunk, make_constant(CONST_NULL, value));
             add_instruction(chunk, CREATE(OP_MOVE, dest, index + 256, 0));
-        } break;
+            return temp;
+        }
 
         case TYPE_STRING:
         {
@@ -653,26 +667,38 @@ void compile_expr(Expression *expr, Chunk *chunk, Scope *scope, int dest) {
 
             int index = add_constant(chunk, make_constant(CONST_STRING, value));
             add_instruction(chunk, CREATE(OP_MOVE, dest, index + 256, 0));
-        } break;
+            return temp;
+        }
 
         case TYPE_BLOCK:
-            compile_list(expr->llist, chunk, scope, dest);
-            break;
+        {
+            return compile_list(expr->llist, chunk, scope, dest, temp);
+        }
     }
+
+    fatal("Could not generate code.");
+    return 0;
 }
 
-void compile_list(ExpressionList *list, Chunk *chunk, Scope *scope, int dest) {
+int compile_list(ExpressionList *list, Chunk *chunk, Scope *scope, int dest, int temp) {
+    int max = temp;
+
     ExpressionNode *head;
     for (head = list->head; head != NULL; head = head->next) {
-        compile_expr(head->expr, chunk, scope, dest);
+        int n = compile_expr(head->expr, chunk, scope, dest, temp);
+        if (n > max) {
+            max = n;
+        }
     }
+
+    return max;
 }
 
 Chunk *compile(Expression *expr) {
     Chunk *chunk = make_chunk();
-    compile_expr(expr, chunk, expr->scope, 0);
+    int max = compile_expr(expr, chunk, expr->scope, 0, 0);
 
-    chunk->numtemps = maxt;
+    chunk->numtemps = max;
     chunk->numlocals = expr->scope->numlocals;
     chunk->numupvars = expr->scope->numupvars;
     chunk->numparams = expr->scope->numparams;
